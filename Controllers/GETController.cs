b75c36d7cdb55679f615ff823969c896c5edd093
Sandeep058 <108403464+Sandeep058@users.noreply.github.com>
2022-07-01 09:41:56 +0530

    GET method

--- Controllers/GETController.cs
+++ Controllers/GETController.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+using System.Data;
+using Library_Management_System.Models;
+
 
 namespace Library_Management_System.Controllers
 {
@@ -7,6 +11,47 @@ namespace Library_Management_System.Controllers
     [ApiController]
     public class GETController : ControllerBase
     {
-        
+        private readonly IConfiguration _configuration;
+
+        public GETController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public IActionResult GET()
+        {
+            string query = @"SELECT * FROM BOOKDETAIL";
+            DataTable dt = new DataTable();
+            string SqlDataSource = _configuration.GetConnectionString("DBConn");
+            SqlDataReader myReader;
+            List<Book> books = new List<Book>();
+
+            using (SqlConnection myConn = new SqlConnection(SqlDataSource))
+            {
+                using (SqlCommand myCommand = new SqlCommand(query, myConn))
+                {
+                    myConn.Open();
+                    SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(myCommand);
+                    sqlDataAdapter.Fill(dt);
+                    foreach (DataRow row in dt.Rows)
+                    {
+                        Book book = new Book()
+                        {
+                            Title = row["Title"].ToString(),
+                            Author = row["Author"].ToString(),
+                            ID = row["ID"].ToString(),
+                            Copyrights = row["Copyrights"].ToString()
+                        };
+                        books.Add(book);
+                    }
+
+                    myConn.Close();
+                    sqlDataAdapter.Dispose();
+                }
+            }
+            return Ok(books);
+        }
+
     }
 }
---
+++ Models/Book.cs
@@ -0,0 +1,10 @@
+﻿namespace Library_Management_System.Models
+{
+    public class Book
+    {
+        public string ID { get; set; }
+        public string Title { get; set; }
+        public string Copyrights { get; set; }
+        public string Author { get; set; }
+    }
+}
